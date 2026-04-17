376d89a8280f36a8b9e86f493c732566c00c0179
i-shashanksharma <shashanksharma240602005@gmail.com>
2026-04-17 15:36:13 +0530

    Delete frontend directory

--- frontend/src/routes/aiRoutes.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { createResumeSummary } from "../controllers/aiController.js";
-
-const router = express.Router();
-
-router.post("/resume-summary", createResumeSummary);
-
-export default router;
