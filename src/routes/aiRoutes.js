b4000877d758515119c8e6361b41130d54bb4f47
Shashank Shekhar <195772358+shashank8536@users.noreply.github.com>
2026-04-17 17:23:29 +0530

    Delete src directory

--- src/routes/aiRoutes.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { enhanceResume } from "../controllers/aiController.js";
-
-const router = express.Router();
-
-router.post("/enhance-resume", enhanceResume);
-
-export default router;
